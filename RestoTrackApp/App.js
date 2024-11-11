7de56309a713980eafab92e1355b3c52d4aa678c
Olivier Dounla <olidounla@gmail.com>
2024-11-10 20:29:48 -0800

    Updated RestoTrackApp with new features and state management

--- RestoTrackApp/App.js
+++ RestoTrackApp/App.js
@@ -7,8 +7,12 @@ import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import SearchScreen from "./src/screens/SearchScreen";
 import DetailScreen from "./src/screens/DetailScreen";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
+import { setNavigator } from "./src/navigationRef";
+import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 
 const switchNavigator = createSwitchNavigator({
+  ResolveAuthScreen: ResolveAuthScreen,
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen,
@@ -24,4 +28,16 @@ const switchNavigator = createSwitchNavigator({
   }),
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App
+        ref={(navigator) => {
+          setNavigator(navigator);
+        }}
+      />
+    </AuthProvider>
+  );
+};
--- RestoTrackApp/src/components/RestaurantsList.js
+++ RestoTrackApp/src/components/RestaurantsList.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { withNavigation } from "react-navigation";
 import RestaurantsDetail from "./RestaurantsDetails";
 
-const RestaurantsList = ({ title, restaurants }) => {
+const RestaurantsList = ({ title, restaurants, navigation }) => {
+  if (!restaurants) {
+    return null;
+  }
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={restaurants}
         keyExtractor={(resto) => resto.id}
         renderItem={({ item }) => {
-          return <RestaurantsDetail resto={item} />;
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RestoDetail", { id: item.id })
+              }
+              style={styles.touchable}
+            >
+              <RestaurantsDetail resto={item} />
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
@@ -19,10 +39,31 @@ const RestaurantsList = ({ title, restaurants }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+  },
   titleStyle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "bold",
+    color: "#333",
+    marginVertical: 10,
+    marginLeft: 15,
+  },
+  listContent: {
+    paddingLeft: 15,
+    paddingRight: 5,
+  },
+  touchable: {
+    marginRight: 10,
   },
 });
 
-export default RestaurantsList;
+export default withNavigation(RestaurantsList);
